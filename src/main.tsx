a9c9000382019ff52d575d6d54cd7c883b0f0dec
Marcos <imlecus@proton.me>
2026-09-19 12:58:00 +0200

    style: applied more padding to the header

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles/app.css";
-import "./styles/theme.css";
 import "./styles/components/graph.css";
 import "./styles/components/chat.css";
 import "./styles/components/settings.css";
 import "./styles/components/node.css";
 import "./styles/components/brain.css";
 import "./styles/components/renderer.css";
-import "./styles/humanize/humanize.css"
+import "./styles/humanize/humanize.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
